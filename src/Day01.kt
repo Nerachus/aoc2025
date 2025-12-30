c51351747ed5274403921f08b5df6da735c1374b
nerachus <nerachus@gmail.com>
2025-12-30 18:32:04 +0100

    First attempt with amor

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,55 @@
 fun main() {
+    val max = 100
     fun part1(input: List<String>): Int {
-        return input.size
+        var position = 50
+        var zeros = 0
+        input.forEach {
+            val direction = it[0]
+            val number = it.substring(1).toInt()
+            if (direction == 'L') {
+                position -= number
+            } else if (direction == 'R') {
+                position += number
+            } else throw IllegalArgumentException("Invalid direction: $direction")
+            position %= max
+            if (position == 0) zeros++
+        }
+        return zeros
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var position = 50
+        var zeros = 0
+        input.forEach {
+            val prevPosition = position
+            val direction = it[0]
+            val number = it.substring(1).toInt()
+            if (direction == 'L') {
+                position -= number
+            } else if (direction == 'R') {
+                position += number
+            } else throw IllegalArgumentException("Invalid direction: $direction")
+            var moveOverZero = 0
+            while (position < 0) {
+                position += max
+                moveOverZero++
+            }
+            while (position >= max) {
+                position -= max
+                moveOverZero++
+            }
+            val equalZero = if (position == 0) 1 else 0
+            val moveOrEqual = if (moveOverZero > 0) moveOverZero else equalZero
+            val startZero = if (moveOrEqual > 0 && prevPosition == 0) -1 else 0
+            zeros += moveOrEqual + startZero
+        }
+        return zeros
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 3)
+    check(part2(testInput) == 6)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
